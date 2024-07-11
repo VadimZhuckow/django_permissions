c749eed0e0e8379cffc156efdbfb28518055e43b
Vadim Zhukov <apexknight@yandex.ru>
2024-07-11 19:17:35 +0300

    s

---
+++ app/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render
+from .models import get_random_text
+from django.http import HttpRequest, JsonResponse
+from django.shortcuts import redirect
+from django.contrib.auth import login, logout, authenticate
+from .forms import TemplateForm, CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+
+def template_view(request):
+    if request.method == "GET":
+        return render(request, 'app/template_form.html')
+
+    if request.method == "POST":
+        received_data = request.POST
+        form = TemplateForm(received_data)
+        if form.is_valid():
+            my_text = form.cleaned_data.get("my_text")
+            my_select = form.cleaned_data.get("my_select")
+            my_textarea = form.cleaned_data.get("my_textarea")
+            my_email = form.cleaned_data.get("my_email")
+            my_password = form.cleaned_data.get("my_password")
+            my_date = form.cleaned_data.get("my_date")
+            my_number = form.cleaned_data.get("my_number")
+            my_checkbox = form.cleaned_data.get("my_checkbox")
+        return render(request, 'app/template_form.html', context={"form": form})
+
+
+def login_view(request):
+    if request.method == "GET":
+        return render(request, 'app/login.html')
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("app:user_profile")
+        return render(request, "app/login.html", context={"form": form})
+
+
+def logout_view(request):
+    if request.method == "GET":
+        logout(request)
+        return redirect("/")
+
+
+def register_view(request):
+    if request.method == "GET":
+        return render(request, 'app/register.html')
+
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # Возвращает сохраненного пользователя из данных формы
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect("app:user_profile")
+
+        return render(request, 'app/register.html', context={"form": form})
+
+
+def index_view(request):
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect("app:user_profile")
+        return render(request, 'app/index.html')
+
+
+def user_detail_view(request):
+    if request.method == "GET":
+        return render(request, 'app/user_details.html')
+
+
+def get_text_json(request):
+    if request.method == "GET":
+        return JsonResponse({"text": get_random_text()},
+                            json_dumps_params={"ensure_ascii": False})
